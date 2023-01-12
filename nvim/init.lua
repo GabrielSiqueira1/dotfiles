6bc18cc477c4189b2ac66b7ba1b0d6cc242d9335
Gabriel Siqueira Silva <gabriel@pop-os.localdomain>
2023-01-11 23:14:24 -0300

    Reinicio

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,3 @@
-vim.cmd('autocmd!')
+-- init.lua
 
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-
-vim.wo.number = true
-
-vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = false
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = 'pwsh'
-vim.opt.inccommand = 'split'
-vim.opt.ignorecase = true
-vim.opt.breakindent = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.ai = true
-vim.opt.si = true
-vim.opt.wrap = false
-vim.opt.backspace = 'start,eol,indent'
-vim.opt.path:append { '**' }
-vim.opt.wildignore:append { '*/node_modules/*' }
+require('settings')	-- lua/settings.lua
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,25 @@
+-- settings.lua
+
+-- buffers
+
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
+
+-- global options
+o.swapfile = true
+o.dir = '/tmp'
+o.smartcase = true
+o.laststatus = 2
+o.hlsearch = true
+o.incsearch = true
+o.ignorecase = true
+o.scrolloff = 12
+-- ... snip ... 
+
+-- window-local options
+wo.number = true
+wo.wrap = false
+
+-- buffer-local options
+bo.expandtab = true
